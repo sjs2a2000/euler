4499588397b85bc50f9031e787a2db6e52870d35
tsmith <scottandsophia@gmail.com>
2019-10-09 09:36:40 -0400

    Create euler.py

---
+++ euler.py
@@ -0,0 +1,18 @@
+import logging
+import argparse
+from InputProcessor import InputFile 
+from OutputProcessor import EulerProcessor
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+ 
+def main():       
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--input',type=str, help='input file', default=r'c:\users\sjs2a\Downloads\triangle_input.txt')
+    args = parser.parse_args()
+    reader = InputFile(args.input)
+    processor = EulerProcessor(reader)
+    processor.process()
+    logger.info("maximum value = {}".format(processor.getMax()))
+     
+if __name__ == "__main__":
+    main()
